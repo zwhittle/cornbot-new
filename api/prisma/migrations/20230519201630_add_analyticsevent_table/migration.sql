dccfc57f153796f59998299435b205ed6772b01d
Zach Whittle <zach@zachwhittle.com>
2023-05-23 00:28:31 -0500

    update db migrations

--- api/prisma/migrations/20230519201630_add_analyticsevent_table/migration.sql
+++ api/prisma/migrations/20230519201630_add_analyticsevent_table/migration.sql
@@ -1,16 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `boosterRole` on the `Guilds` table. All the data in the column will be lost.
-  - You are about to drop the column `comboRole` on the `Guilds` table. All the data in the column will be lost.
-  - You are about to drop the column `twitchRole` on the `Guilds` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Guilds" DROP COLUMN "boosterRole",
-DROP COLUMN "comboRole",
-DROP COLUMN "twitchRole";
-
 -- CreateTable
 CREATE TABLE "AnalyticsEvent" (
     "id" SERIAL NOT NULL,
